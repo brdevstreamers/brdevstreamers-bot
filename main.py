355c1db47646705c0d43f1b3809f8fc1e61e043c
Mendes <flaviojmendes@gmail.com>
2022-03-01 01:37:58 +0000

    adding exception handling twit

--- main.py
+++ main.py
@@ -21,12 +21,15 @@ def job():
     streams = get_streams()
     for stream in streams:
         if stream['user_name'] not in streams_map or streams_map[stream['user_name']] != stream['started_at']:
-            streams_map[stream['user_name']] = stream['started_at']
-            tweet_stream(stream)
-            print(f'Twitting {stream["user_name"]} stream...')
+            try:
+                streams_map[stream['user_name']] = stream['started_at']
+                tweet_stream(stream)
+                print(f'Twitting {stream["user_name"]} stream...')
+            except Exception as e:
+                print(f'Couldnt Twit {stream["user_name"]} stream...')
 
 
-schedule.every(10).seconds.do(job)
+schedule.every(5).minutes.do(job)
 
 while True:
     schedule.run_pending()
